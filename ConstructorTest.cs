bbdd13aacae797bf91883a250bfa62e96b07dc77
Kolvinkst <einbenutzer1998@gmail.com>
2023-12-13 22:28:17 +0700

    Добавьте файлы проекта.

---
+++ ConstructorTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konskruktor
+{
+    internal class ConstructorTest
+    {
+        public int i = 1;
+        public ConstructorTest()
+        {
+            Console.WriteLine($"i is {i}");
+        }
+    }
+}
